22dc1a1466ee03ff30c9858e151b82a17f8e849c
Ayush Sharma <ayushs.sharma@vayuz.com>
2025-05-07 12:10:24 +0530

    Checking meta tags working when static

--- src/components/layout/Title.jsx
+++ src/components/layout/Title.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Title = () => {
-    const location = useLocation();
-    const path = location.pathname;
 
+    let location = useLocation();
+    console.log("location", location.pathname);
+    let path = location.pathname
+
+    // It sets title for each components or page according to the pathname 
     useEffect(() => {
+
         if (path.includes('/login')) {
             document.title = "Login | CRUD";
         } else if (path.includes('/signup')) {
@@ -13,29 +17,28 @@ const Title = () => {
         } else if (path.includes('/home')) {
             document.title = "Home | CRUD";
         } else if (path.includes('/about')) {
-            document.title = "About | CRUD";
+            document.title = "About | CRUD"
         } else if (path.includes('/createUser')) {
-            document.title = "Create User | CRUD";
+            document.title = "Create User | CRUD"
         } else if (path.includes('/updateUser')) {
-            document.title = "Update User | CRUD";
+            document.title = "Update User | CRUD"
         } else if (path.includes('/userDetails')) {
-            document.title = "User Details | CRUD";
+            document.title = "User Details | CRUD"
         }
 
-        const titleMeta = document.querySelector('meta[property="og:title"]');
-        const descriptionMeta = document.querySelector('meta[property="og:description"]');
+        // if (path.includes("/home")) {
+        //     document.querySelector('meta[name="title"]')?.setAttribute("content", "CRUD Application.");
+        //     document.querySelector('meta[name="description"]')?.setAttribute("content", "User Management.");
 
-        if (titleMeta && descriptionMeta) {
-            if (path.includes('/home')) {
-                titleMeta.setAttribute("content", "CRUD Application");
-                descriptionMeta.setAttribute("content", "User Management");
-            } else if (path.includes('/about')) {
-                titleMeta.setAttribute("content", "About the Developer | CRUD Application");
-                descriptionMeta.setAttribute("content", "Simple Introduction");
-            }
-        }
-    }, [path]); 
-    return null;
-};
+        // } else if (path.includes("/about")) {
+        //     document.querySelector('meta[name="title"]')?.setAttribute("content", "About the Developer | CRUD Application.");
+        //     document.querySelector('meta[name="description"]')?.setAttribute("content", "Simple Introduction");
 
-export default Title;
+        // }
+
+    }, [location.pathname]);
+
+    return null; 
+}
+
+export default Title
